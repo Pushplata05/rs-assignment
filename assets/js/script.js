30c85d5ee877eab5886f67dec5e7adc22cf93927
Pushplata <pushplata@infostride.com>
2023-10-17 17:08:09 +0530

    assessible page

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function() {
+    $("#news-slider").owlCarousel({
+        items : 5,
+        itemsDesktop:[1199,3],
+        itemsDesktopSmall:[980,2],
+        itemsMobile : [600,1],
+        navigation:true,
+        navigationText:["",""],
+        pagination:true,
+        autoPlay:true
+    });
+});
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  
