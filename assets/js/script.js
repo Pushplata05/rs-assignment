e1f53a28b3ccaa923b2377e11b38c02551f54f96
Pushplata <pushplata@infostride.com>
2023-11-23 19:26:14 +0530

    scripts for hide and show

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,27 @@
 var menuObject = {
   bigger_cursor: false,
   show_images: false,
+  show_invert: false,
   show_mask: false,
+  show_line: false,
+  show_changefont: false,
+  increase_font: false,
+  decrease_font:false,
+  initialState:{
+    font: 28,
+    brightness: 100,
+    contrast: 100,
+    saturation: 100,
+    grayscaleValue: 0,
+    lineheight: 1.5,
+    backgroundColor: "white",
+    fontColor: "black",
+    fontFamily: "'Zilla Slab', serif",
+  }
 };
 
 // Initial state
-var initialState = {
+const initialState = {
   font: 28,
   brightness: 100,
   contrast: 100,
@@ -16,7 +32,7 @@ var initialState = {
   fontColor: "black",
   fontFamily: "'Zilla Slab', serif",
 };
-var tempstate = initialState;
+let tempstate = initialState;
 
 function showReadingMask() {
   const myDivTop = $(".accessibly-app-reading-mask-top");
@@ -60,22 +76,39 @@ function showReadingMask() {
 }
 
 
-// ------------------------------Font-btn-1-------------------------------
+// Function to update font styles
+function updateFontStyles() {
+  $("body, h1, p").attr("style", `font-family: ${tempstate.fontfamily}; font-size: ${tempstate.font}px !important;`);
+}
+
+// Font size increment/decrement logic
 $(".modal-btn1").on("click", function () {
-  tempstate.font += 5;
-  $("body").attr(
-    "style",
-    `font-family: ${tempstate.fontFamily}; font-size: ${tempstate.font}px !important;`
-  );
-  $("h1").attr(
-    "style",
-    `font-family: ${tempstate.fontFamily}; font-size: ${tempstate.font}px !important;`
-  );
-  $("p").attr(
-    "style",
-    `font-family: ${tempstate.fontFamily}; font-size: ${tempstate.font}px !important;`
-  );
+  // Set menuObject.increase_font based on the current font size
+  menuObject.increase_font = tempstate.font > 50 ? false : true;
+  if (tempstate.font > 50 && menuObject.decrease_font) {
+    menuObject.increase_font = false;
+  } else {
+    menuObject.increase_font = true;
+    menuObject.decrease_font = true;
+  }
+  
+  console.log(tempstate.font, menuObject.increase_font, menuObject.decrease_font)
+
+  if (menuObject.increase_font) {
+    if (tempstate.font < 50) {
+      tempstate.font += 5;
+      $(".modal-btn1").html(`<iconify-icon icon="fluent:line-horizontal-1-24-filled"></iconify-icon>Increase Font`);
+    }
+  } else {
+    if (tempstate.font > 28) {
+      tempstate.font -= 5;
+      $(".modal-btn1").html(`<iconify-icon icon="fluent:line-horizontal-1-24-filled"></iconify-icon>Decrease Font`);
+    }
+  }
+
+  updateFontStyles();
 });
+
 
 // ------------------------------Currsor-btn-2-------- bigger_curso : true/ false-----------------------
 $(".modal-btn2").on("click", function () {
@@ -92,40 +125,69 @@ $(".modal-btn2").on("click", function () {
 $(".modal-btn3").on("click", function () {
   menuObject.show_images = !menuObject.show_images;
   if (menuObject.show_images) {
-    $(".modal-btn3").text("Show Images")
+    $(".modal-btn3").html(`<iconify-icon icon="ion:image-sharp"></iconify-icon>Show Images`)
     $("img").attr("style", `display: none !important;`);
   } else {
-    $(".modal-btn3").text("Hide Images")
+    $(".modal-btn3").html(`<iconify-icon icon="ion:image-sharp"></iconify-icon>Hide Images`)
     $("img").attr("style", `display: inline-block !important;`);
   }
 
 });
 
-$(".reading-line").on("click", function () { 
-  $(".accessibility_line").css("display", "block");
-  $(document).on("mousemove", function (e) {
-    var scrollPosition = $(window).scrollTop();
-    $(".accessibility_line").css("top", e.clientY + "px");
-  });
+// $(".reading-line").on("click", function () { 
+//   $(".accessibility_line").css("display", "block");
+//   $(document).on("mousemove", function (e) {
+//     var scrollPosition = $(window).scrollTop();
+//     $(".accessibility_line").css("top", e.clientY + "px");
+//   });
+// });
+$(".modal-btn.reading-line").on("click", function () {
+  menuObject.show_line = !menuObject.show_line;
+
+  if (menuObject.show_line) {
+      // Show the reading line
+      $(this).html(`<iconify-icon icon="fluent:line-horizontal-1-24-filled"></iconify-icon>Reading Line`);
+  } else {
+      // Hide the reading line
+      $(this).html(`<iconify-icon icon="fluent:line-horizontal-1-24-filled"></iconify-icon>Remove Line`);
+  }
+
+  // Toggle the visibility of the accessibility_line
+  $(".accessibility_line").toggle(menuObject.show_line);
+
+  if (menuObject.show_line) {
+      // Set up the reading line position on mousemove
+      $(document).on("mousemove", function (e) {
+          var scrollPosition = $(window).scrollTop();
+          $(".accessibility_line").css("top", e.clientY + "px");
+      });
+  } else {
+      // Unbind the mousemove event if line is removed
+      $(document).off("mousemove");
+  }
 });
 
+
 // -------------------------------changefont--------------------------------------
+
 $(".changefont").on("click", function () {
-  tempstate.fontfamily = "'Zilla Slab', serif";
-  tempstate.font = 42;
-  $("body").attr(
-    "style",
-    `font-family: ${tempstate.fontfamily}; font-size: ${tempstate.font}px !important;`
-  );
-  $("h1").attr(
-    "style",
-    `font-family: ${tempstate.fontfamily}; font-size: ${tempstate.font}px !important;`
-  );
-  $("p").attr(
-    "style",
-    `font-family: ${tempstate.fontfamily}; font-size: ${tempstate.font}px !important;`
-  );
+  menuObject.show_changefont = !menuObject.show_changefont;
+
+  if (menuObject.show_changefont) {
+      // Change to normal font
+      $(".changefont").html(`<iconify-icon icon="ion:image-sharp"></iconify-icon>Normal Font`);
+      tempstate.fontfamily = "'Zilla Slab', serif";
+      tempstate.font = 42;
+
+      $("body, h1, p").attr("style", `font-family: ${tempstate.fontfamily}; font-size: ${tempstate.font}px !important;`);
+  } else {
+      // Change to dyslexic font
+      $(".changefont").html(`<iconify-icon icon="fluent:line-horizontal-1-24-filled"></iconify-icon>Dyslexic Font`);
+      console.log(initialState)
+      $("body, h1, p").attr("style", `font-family: ${menuObject.initialState.fontfamily}; font-size: ${menuObject.initialState.font}px !important;`);
+  }
 });
+
 
 // ---------------------------------change-fontend-------------
 $(".brightness").on("click", function () {
@@ -144,6 +206,8 @@ $(".invert").on("click", function () {
   $("li").css("color", "#ffffff");
   $("p").attr("style", `color: #ffffff;`);
 });
+
+
 $(".lineheight").on("click", function () {
   tempstate.lineheight += 0.1;
   $("body").css("line-height", tempstate.lineheight);
